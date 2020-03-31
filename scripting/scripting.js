f0cd6c178b26119b7d1af9567cd4dd0234fa5ba9
Ranjodh Singh <ranjodhsingh24@gmail.com>
2020-03-31 01:57:34 -0400

    worked on UI and DRY code

--- scripting/scripting.js
+++ scripting/scripting.js
@@ -1,4 +1,5 @@
 
+// load script when page is ready
 (function () {
     // variable declaration
     const maxStage = 6;
@@ -12,12 +13,16 @@
     let userSelectionArray = [];
     let containerWidth = 300;
     
+    // functions definitions
+    // look for how many boxes will be there for user to guess
     const boxes = ()=> {
         if (stage === 3) { boxToSelect = stage; containerWidth = 420;}
         if (stage === 4) { boxToSelect = stage + 1; containerWidth = 540;}
         if (stage === 5) { boxToSelect = stage + 3; containerWidth = 660;}
         if (stage === 6) { boxToSelect = stage + 5; containerWidth = 780;}
     };
+
+    // set the grid structure dynamically as per the number of boxes
     function setGridSizing(){
         document.documentElement.style.setProperty(`--gridColumns`, `${stage-1}`);
         document.documentElement.style.setProperty(`--containerWidth`, `${containerWidth}px`);
@@ -26,18 +31,16 @@
         document.documentElement.style.setProperty(`--boxWidth`, `${boxWidth}px`);
         document.documentElement.style.setProperty(`--boxHeight`, `${boxHeight}px`);
     };
-    
-    // your page initialization code here
-    // the DOM will be available here
-    
-    // functions definitions
+
+    // start the game for any given stage
     const start = function () {
         reset();
         boxes();
-        totalBoxes = stage * stage;
         setGridSizing();
         updateCounter(boxToSelect,stage-2);
-        $('.boxContainer').empty();
+        // re-calculate total boxes as per the stage
+        totalBoxes = stage * stage;
+        // add boxes to the container
         for (let x = 1; x <= totalBoxes; x++) {
             $('.boxContainer')
             .append(`<div class="box" id=\"box${x}\" data-num=\"${x}\">
@@ -48,22 +51,21 @@
                     </div>`
                 );
         }
+        // when the boxes are in place then call the function to show boxes to be selected
         makeRandomSelections();
         showRandomSelections();
     };
     
+    // make the unique random selections out of the given boxes for user to guess
     const makeRandomSelections = function(){
         while (randomBoxSelection.length != boxToSelect) {
             let r = Math.floor(Math.random() * (totalBoxes)) + 1;
-            
             if (randomBoxSelection.indexOf(r) === -1) randomBoxSelection.push(r);
         }
     }
     
+    // show the green color on the random boxes for user to remember
     const showRandomSelections = function(){
-        for (let i = 0; i < randomBoxSelection.length; i++)
-            console.log(randomBoxSelection[i]);
-        
         for (let j = 0; j < randomBoxSelection.length; j++) {
             let temporary = '';
             temporary = '#box' + randomBoxSelection[j];
@@ -76,33 +78,26 @@
     }
 
 
+    // reset the stage for next of previous stage to appear
     const reset = function () {
-        for (let j = 0; j < randomBoxSelection.length; j++) {
-            let temporary = '';
-            temporary = '#box' + randomBoxSelection[j];
-            $(temporary).find('.boxFront').removeClass('boxRandom');
-            $(temporary).find('.boxBack').removeClass('boxRandomBack');
-        }
+        $('.boxContainer').empty();
         randomBoxSelection = [];
         userSelectionArray = [];
     };
 
-
-    
+    // perform actions if user clicks any box to play
     const boxClicked = function (e) {
-        const selection = parseInt($(e.target).closest('.box').attr('data-num'));
+        e.preventDefault();
+        const selection = parseInt($(this).closest('.box').attr('data-num'));
         const selectionIndex = randomBoxSelection.indexOf(selection);
         
-        console.log($(e.target).closest('.box'));
+        $(this).closest('.box').addClass('active');
+        console.log($(this).closest('.box'));
 
-        $(e.target).closest('.box').addClass('active');
         if (selectionIndex !== -1) {
             if (!$(this).hasClass("boxSelected")) {
-                console.log("Right choice...Play again");
                 userSelectionArray.push(selection);
                 $(this).addClass("boxSelected");
-            } else {
-                alert("Already selected");
             }
         } else {
             stageDiminish();
@@ -110,9 +105,11 @@
         updateCounter((boxToSelect - userSelectionArray.length),stage-2);
         if(userSelectionArray.length === boxToSelect){
             stageProgress();
+            // start();
         }
     };
     
+    // if user wins, user will go one stage up
     const stageProgress = function(){
         if(stage < maxStage){
             alert("You won");
@@ -123,6 +120,8 @@
 
         start();
     }
+
+    // if user loses, user will go one stage down
     const stageDiminish = function(){
         if(stage > minStage){
             alert("Wrong answer...Please try again");
@@ -130,21 +129,30 @@
         }
         else
             alert("Please try again");
-        
         start();
     }
+
+    // update the game info counter
     const updateCounter = function(boxes,stage){
         $('.selectionsLeft span').text(boxes);
         $('.stage span').text(stage);
-
     }
 
-    $('#play').on('click', function (e) {
+    // put a delay on the reset button once it is clicked
+    const resetClickDelay = function (e) {
         e.preventDefault();
         start();
-    });
-
+        const $this = $(this);
+        $this.prop("disabled", true);
+        setTimeout(function () {
+            $this.prop("disabled", false);
+        }, 5000);
+    }
+    
     // define events here
-    start();
+    $('#reset').on('click', resetClickDelay);
     $('.boxContainer').on('click',".boxFront",boxClicked);
+    
+    // start the game with initial stage or minStage
+    start();
 })();
